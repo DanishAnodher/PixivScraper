a20411fde8134ac9b2f17faedc95a51469f32fc1
Code Senpai <55613161+CodeSenpaiii@users.noreply.github.com>
2021-10-23 15:00:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ def main():
     illustrations = getImageIds(ArtistID)
     total_illusts = len(illustrations)
     print(f"[INFO] Illustrations Found = {total_illusts}")
+    print(f"[NOTE] Please Wait...")
     for index, illusts in enumerate(illustrations):
         images = getImageLink(illusts, ArtistID)
         for img in images:
             img_name = img.split('/')[-1]
-            download(img, ArtistID, img_name)
-            print(f"[INFO] Downloaded Image {img_name} ({index+1}/{total_illusts})")
+            if isDuplicate(img_name, ArtistID):
+                print(f"[INFO] Image Already Exists! {img_name} ({index+1}/{total_illusts})")
+            else:
+                download(img, ArtistID, img_name)
+                print(f"[INFO] Downloaded Image {img_name} ({index+1}/{total_illusts})")
 
 if __name__ == "__main__":
     main()
-    
